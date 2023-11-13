5d433bd29d669903614c39bcbe0ea777b1117e52
Robi Nurhidayat <robivanpersie290899@gmail.com>
2023-11-13 16:12:36 +0700

    add user id response in job

--- jobs/formatter.go
+++ jobs/formatter.go
@@ -2,6 +2,7 @@ package jobs
 
 type JobsFormatter struct {
 	Id          int    `json:"id"`
+	UserId      int    `json:"user_id"`
 	Company     string `json:"company"`
 	Position    string `json:"position"`
 	Status      string `json:"status"`
@@ -14,6 +15,7 @@ func FormatterJob(job Jobs) JobsFormatter {
 	formatter := JobsFormatter{}
 
 	formatter.Id = job.Id
+	formatter.UserId = job.UserId
 	formatter.Company = job.Company
 	formatter.Position = job.Position
 	formatter.Status = job.Status
